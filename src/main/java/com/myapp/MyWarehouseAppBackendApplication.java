9a175f8ba83b60b972af5580569344501c09be05
Kartik Kocher <kocher.kartik@gmail.com>
2023-12-29 19:40:38 +0530

    added .ebextentions file for Beanstalk and modify MyWarehouseAppBackendApplication.java and pom.xml

--- src/main/java/com/myapp/MyWarehouseAppBackendApplication.java
+++ src/main/java/com/myapp/MyWarehouseAppBackendApplication.java
@@ -2,12 +2,19 @@ package com.myapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyWarehouseAppBackendApplication {
+public class MyWarehouseAppBackendApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyWarehouseAppBackendApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyWarehouseAppBackendApplication.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyWarehouseAppBackendApplication.class, args);
+    }
 
 }
